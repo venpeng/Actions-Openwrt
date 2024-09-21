7e657d1369cabf2d34cf09dac187dc4c6418924f
venpong <venpong.x@gmail.com>
2024-09-21 11:25:55 +0800

    init branch multibuild

--- diysh/acrh17/11.1/diy-part2.sh
+++ diysh/acrh17/11.1/diy-part2.sh
@@ -20,7 +20,7 @@
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 # 修改wifi名称
-sed -i 's/ImmortalWrt/ASUS/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/ASUS/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 下载ServerChan
 #git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
