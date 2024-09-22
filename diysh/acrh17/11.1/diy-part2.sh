0e1e98d2b61a1cbb9f8da8a2b02b2b035bd7b464
venpong <venpong.x@gmail.com>
2024-09-22 19:57:16 +0800

    version=R24.9.22,sinbox=1.9.6

--- diysh/acrh17/11.1/diy-part2.sh
+++ diysh/acrh17/11.1/diy-part2.sh
@@ -10,21 +10,29 @@
 # See /LICENSE for more information.
 #
 
-# 修改默认主题
+#定义变量
+CFG_FILE="./package/base-files/files/bin/config_generate"
+WRT_IP="192.168.11.1"
+WRT_WIFI="ASUS"
+WRT_NAME="Openwrt"
+
+#修改默认主题
+#sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 修改主机名称
+#修改主机名称
+#sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 # 修改网关地址
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
+#sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+
+#修改immortalwrt.lan关联IP-immortalwrt才有flash.js,用于luci页面-->系统-->备份与升级页面操作完成之后,重新刷新luci页面
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+#sed -i 's/192.168.1.1/192.168.11.1/g' feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
 
 # 修改wifi名称
-sed -i 's/ImmortalWrt/ASUS/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# 下载ServerChan
-#git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
-
-#echo '集成diy目录'
-#ln -s ../../diy ./package/openwrt-packages
+#sed -i 's/ImmortalWrt/ASUS/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/\.ssid=.*/\.ssid=$WRT_WIFI/g" $(find ./package/kernel/mac80211/ ./package/network/config/ -type f -name "mac80211.*")
 
